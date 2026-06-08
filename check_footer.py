513372e012a2f8e29cb2346ce65c4c371fa5018e
HimaVamsi G <himavamsigumpu@gmail.com>
2026-06-08 22:04:19 +0530

    feat: add new service pages, update navigation and site styling, and remove deprecated build scripts

--- check_footer.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-match = re.search(r'<h2 class="widget-title">Contact Us</h2>.*?<ul class="pbmit-list-items">(.*?)</ul>', text, re.DOTALL)
-if match:
-    print(match.group(1))
